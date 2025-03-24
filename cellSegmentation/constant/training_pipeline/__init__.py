216fb48c26992231e66d013060aed31ba6c65b52
viplove <viplovethakran4@gmail.com>
2025-03-24 16:50:13 +0530

    data validation added

--- cellSegmentation/constant/training_pipeline/__init__.py
+++ cellSegmentation/constant/training_pipeline/__init__.py
@@ -9,3 +9,14 @@ DATA_INGESTION_DIR_NAME: str = 'data_ingestion'
 DATA_INGESTION_FEATURE_STORE_DIR: str = 'feature_store'
 
 DATA_DOWNLOAD_URL: str = 'https://drive.google.com/file/d/1EXGLpbaGsl_JxCnjvPplb7wUYVEnIp0K/view?usp=sharing'
+
+
+'''
+Data Validation realted contant start with Data_Validation VAR name
+'''
+
+DATA_VALIDATION_DIR_NAME:str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES= ['train', 'valid', 'test', 'data.yaml']
